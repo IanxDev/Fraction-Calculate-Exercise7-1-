4bc8c918cc98dc1abfa5bd821ca0a13af7b9de26
Verinda <gen.intertsingtao@gmail.com>
2014-12-11 21:13:55 +0800

    Initial Commit

---
+++ Exercise7-1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Exercise7-1
+//
+//  Created by Ian Wu on 14/12/11.
+//  Copyright (c) 2014年 Ian. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
